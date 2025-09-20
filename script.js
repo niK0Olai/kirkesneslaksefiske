cb7f37d66cefb294cd735bf44eb44f1c070b094f
Nikolai Takle Iselvmo <143708757+niK0Olai@users.noreply.github.com>
2025-09-20 14:22:27 +0200

    Moved form script

--- script.js
+++ script.js
@@ -100,17 +100,4 @@ function loadAnalytics() {
 
 
 
-    //FORM SUBMISSION
-  document.getElementById("dataForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
-    const input = e.target.elements.data.value;
-    
-    const response = await fetch("/process", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ input })
-    });
 
-    const result = await response.text();
-    alert("Server says: " + result);
-});
